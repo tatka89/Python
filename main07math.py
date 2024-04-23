17aaa13b89541453f2bf870848b3cd2828faff36
Nataly <tatulkaaa@gmail.com>
2024-04-23 11:28:35 +0300

    Результаты 2 дня обучения

---
+++ main07math.py
@@ -0,0 +1,6 @@
+from math import sqrt
+
+n = input('Введите целое число:') or 25
+n = float(n)
+
+print('Квадратный корень из числа ' + str(n) +' равно ' + str(sqrt(n)))
---
+++ main08rand.py
@@ -0,0 +1,11 @@
+#import random
+
+from random import *
+
+#x = randint(1,6)
+#y = randint(1,6)
+
+#z = random()
+n = 5
+m = 6
+print(randint(n,m),randint(n,m),random(), sep = "\t")
