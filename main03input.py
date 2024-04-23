771a04dcb13dbd813765a719ee32b00518ac543b
Nataly <tatulkaaa@gmail.com>
2024-04-23 09:48:16 +0300

    Результаты 1 дня обучения

---
+++ main03input.py
@@ -0,0 +1,14 @@
+a = input()
+b = input()
+
+c = a + b
+
+print('Сумма числе(строк):',c)
+
+
+a1 = int(a)
+b1 = int(b)
+c1 = a1 + b1
+
+
+print('Сумма числе: ',c1)
---
+++ main04plus.py
@@ -0,0 +1,5 @@
+print( 'Введите два числа:' )
+a = int (input('a = '))
+b = int (input('b = '))
+c = a + b
+print('a + b = ' + str(a)+' + '+str(b)+' = '+str(c))
